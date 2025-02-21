866d8964f5f9c9c8f98d2612628261bd7f2abf89
KimThanh1801 <hokimthanh1234@gmail.com>
2025-02-21 15:33:06 +0700

    update css of api

--- HomeWork/resources/views/api.blade.php
+++ HomeWork/resources/views/api.blade.php
@@ -6,6 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        table,th,tr td{
+            border: 1px solid #000;
+            padding: 10px;
+        }
+        td{
+            background-color: #ffc0cb;
+        }
+    </style>
 </head>
 
 <body>
